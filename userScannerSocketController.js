5c96dd72bef1fc00554d724857c7fe8f3d92b985
Frank Poon <fpooooon@gmail.com>
2017-12-04 20:10:10 -0800

    fix websocket?

--- userScannerSocketController.js
+++ userScannerSocketController.js
@@ -3,7 +3,7 @@ var Socket={						//the socket object
 	setup: function (){
 				logConsole("setup Called");
 				//var host = "ws: //127.0.0.1: 8080/ws";
-				var host = "wss: //subredditscannerserver.herokuapp.com/";
+				var host = "wss://subredditscannerserver.herokuapp.com/";
 				socket = new WebSocket(host);
 				if (socket) {
 					socket.onopen = function() {
